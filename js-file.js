2106e05b56617ded28b77e6bae42fac54cf6581c
EnharmonicDude <201596891+EnharmonicDude@users.noreply.github.com>
2025-04-15 09:11:05 +0200

    Even more DOM manipulation

--- js-file.js
+++ js-file.js
@@ -1,4 +1,23 @@
 // your JavaScript file
+/*Add the following elements to the container using ONLY JavaScript and the DOM methods shown above:
+
+a <p> with red text that says “Hey I’m red!”
+an <h3> with blue text that says “I’m a blue h3!”
+a <div> with a black border and pink background color with the following elements inside of it:
+another <h1> that says “I’m in a div”
+a <p> that says “ME TOO!”
+Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container. 
+
+//creates new Element
+document.createElement(tagName, [options])
+//inline CSS
+div.style.cssText = "color: blue; background: white;"
+//use camelCase
+div.style.backgroundColor
+// adds class "new" to your new div
+div.classList.add("new");
+
+*/
 const container = document.querySelector("#container");
 
 const content = document.createElement("div");
@@ -29,22 +48,31 @@ divPinkBorder.style.backgroundColor = "pink";
     divPinkBorder.appendChild(pInDiv);
 
 container.appendChild(divPinkBorder);
-/*Add the following elements to the container using ONLY JavaScript and the DOM methods shown above:
 
-a <p> with red text that says “Hey I’m red!”
-an <h3> with blue text that says “I’m a blue h3!”
-a <div> with a black border and pink background color with the following elements inside of it:
-another <h1> that says “I’m in a div”
-a <p> that says “ME TOO!”
-Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container. 
+// the JavaScript file
+const btn = document.querySelector("#btn");//querySelector finds the id of the button and assigns it to the JS constant
+btn.addEventListener("click", () => {
+  alert("Hello World"); //we add an Eventlistener which gets activsted on a click and then a method of our choice
+});
 
-//creates new Element
-document.createElement(tagName, [options])
-//inline CSS
-div.style.cssText = "color: blue; background: white;"
-//use camelCase
-div.style.backgroundColor
-// adds class "new" to your new div
-div.classList.add("new");
+btn.addEventListener("click", function (e) {
+    console.log(e); //this gives us a console log that contains all information about that particular Event (target, timestamp, offset, angle etc.)
+  });
 
-*/
+  btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue"; //turns the button blue on click
+  });
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
+
+  
+  
